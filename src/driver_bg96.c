7db31fa93ec66cacc214c2e371c49a42b708a38f
Mario Aguilar <fernandoaguilar731010@gmail.com>
2022-12-03 16:19:05 -0400

    se aumento el tamaño de los buffer de open y pub

--- src/driver_bg96.c
+++ src/driver_bg96.c
@@ -277,8 +277,8 @@ em_bg96_error_handling close_client_mqtt(st_bg96_config *self)
 em_bg96_error_handling connect_server_mqtt(st_bg96_config *self)
 {
     em_bg96_error_handling ft_resp=FT_BG96_ERROR;
-    char buffer_resp[30]={0}; 
-    char cmd[50]={0};
+    char buffer_resp[150]={0}; 
+    char cmd[150]={0};
     sprintf(cmd,"AT+QMTCONN=%u,\"%s\",\"%s\",\"%s\"\r",self->self_mqtt.identifier_socket_mqtt,self->self_mqtt.mqtt_client_id,self->self_mqtt.mqtt_username,self->self_mqtt.mqtt_password);
     ft_resp=self->send_data_device(cmd,RS_BG96_CERO,buffer_resp,5000);
     if (ft_resp!=FT_BG96_OK)
@@ -306,7 +306,7 @@ em_bg96_error_handling disconnect_server_mqtt(st_bg96_config *self)
 em_bg96_error_handling publish_message(st_bg96_config *self,char *topic,char *data)
 {
     em_bg96_error_handling ft_resp=FT_BG96_ERROR;
-    char buffer_resp[30]={0}; 
+    char buffer_resp[120]={0}; 
     char cmd[50]={0};
     char buffer_data[120]={0};
     sprintf(buffer_data,"%s\x1a\r",data);
@@ -338,10 +338,6 @@ em_bg96_error_handling turn_off_bg96(st_bg96_config *self)
 
 }
 
-em_bg96_error_handling raisen_mqtt_server(st_bg96_config *self)
-{
-    
-}
 em_bg96_error_handling send_data_mqtt(st_bg96_config *self,char *topic,char *data)
 {
     em_bg96_error_handling ft_resp=FT_BG96_OK;
